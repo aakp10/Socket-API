d514d8fa8717f363cee8405c14908b900c4e08bd
Karuna Grewal <karunagrewal98@gmail.com>
2018-09-04 21:58:31 +0530

    Some trivial pipe

---
+++ minimal-pipe/trivial-pipes.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+#define P_READ 0
+#define P_WRITE 3
+#define C_WRITE 1
+#define C_READ 2
+
+int main()
+{
+    int pipe_fd[4];
+    pid_t child_pid;
+    int value;
+    
+    pipe(pipe_fd);
+    pipe(pipe_fd + 2);
+
+
+    if((child_pid == fork()) == 0)
+    {
+        /* close parent related ends of the pipe */
+        close(pipe_fd[P_READ]);
+        close(pipe_fd[P_WRITE]);
+        /* receive value from the parent */
+        int count_read;
+        if (read(pipe_fd[C_READ], &value, sizeof(value)) > 0) {
+            printf("child (%d) : read %d \n", getpid(), value);
+        }
+
+        value *= 2;
+        /* write this value to parent after processing */
+        write(pipe_fd[C_WRITE], &value, sizeof(value));
+        printf("child(%d): wrote %d\n", getpid(), value);
+        exit(0);        
+    }
+    else {
+        close(pipe_fd[C_WRITE]);
+        close(pipe_fd[C_READ]);
+
+        value = 10;
+        printf("parent(%d): wrote %d\n", getpid(), value);
+        write(pipe_fd[P_WRITE], &value, sizeof(value));
+        if(read(pipe_fd[P_READ], &value, sizeof(value)) > 0) {
+            printf("parent(%d): read %d\n", getpid(), value);
+        }
+        wait(NULL);
+    }
+    return 0;
+}
